d45ec00162a2de6ebd515c3c8be27e1ceccb8aa5
Enstientt <76008179+Enstientt@users.noreply.github.com>
2023-05-16 22:22:56 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -5,8 +5,10 @@
 #include </Users/zessadqu/Desktop/MLX42/include/MLX42/MLX42.h>
 #include <math.h>
 
-#define WIDTH 576
-#define HEIGHT 576
+#define WIDTH 800
+#define HEIGHT 800
+#define MWIDTH 400
+#define MHEIGHT 400
 
 typedef struct player
 {
@@ -19,112 +21,35 @@ typedef struct player
 	float rot_speed;
 	char	**map;
 	mlx_t	*mlx;
+	mlx_image_t * image;
+	int *size;
+	int  raduis;
+	int minX ;
+	int minY;
 } t_player;
-// static mlx_image_t* image;
-
-// // -----------------------------------------------------------------------------
-
-// int32_t ft_pixel(int32_t r, int32_t g, int32_t b, int32_t a)
-// {
-//     return (r << 24 | g << 16 | b << 8 | a);
-// }
-
-// void ft_randomize(void* param)
-// {
-// 	for (int32_t i = 0; i < image->width; ++i)
-// 	{
-// 		for (int32_t y = 0; y < image->height; ++y)
-// 		{
-// 			uint32_t color = ft_pixel(
-// 				rand() % 0xFF, // R
-// 				rand() % 0xFF, // G
-// 				rand() % 0xFF, // B
-// 				rand() % 0xFF  // A
-// 			);
-// 			mlx_put_pixel(image, i, y, color);
-// 		}
-// 	}
-// }
-
-// void ft_hook(void* param)
-// {
-// 	mlx_t* mlx = param;
-
-// 	if (mlx_is_key_down(mlx, MLX_KEY_ESCAPE))
-// 		mlx_close_window(mlx);
-// 	if (mlx_is_key_down(mlx, MLX_KEY_UP))
-// 		image->instances[0].y -= 5;
-// 	if (mlx_is_key_down(mlx, MLX_KEY_DOWN))
-// 		image->instances[0].y += 5;
-// 	if (mlx_is_key_down(mlx, MLX_KEY_LEFT))
-// 		image->instances[0].x -= 5;
-// 	if (mlx_is_key_down(mlx, MLX_KEY_RIGHT))
-// 		image->instances[0].x += 5;
-// }
-
-// // -----------------------------------------------------------------------------
-
-// int32_t main(int32_t argc, const char* argv[])
-// {
-// 	mlx_t* mlx;
-
-// 	// Gotta error check this stuff
-// 	if (!(mlx = mlx_init(WIDTH, HEIGHT, "MLX42", true)))
-// 	{
-// 		puts(mlx_strerror(mlx_errno));
-// 		return(EXIT_FAILURE);
-// 	}
-// 	if (!(image = mlx_new_image(mlx, 128, 128)))
-// 	{
-// 		mlx_close_window(mlx);
-// 		puts(mlx_strerror(mlx_errno));
-// 		return(EXIT_FAILURE);
-// 	}
-// 	if (mlx_image_to_window(mlx, image, 0, 0) == -1)
-// 	{
-// 		mlx_close_window(mlx);
-// 		puts(mlx_strerror(mlx_errno));
-// 		return(EXIT_FAILURE);
-// 	}
-	
-// 	mlx_loop_hook(mlx, ft_randomize, mlx);
-// 	mlx_loop_hook(mlx, ft_hook, mlx);
-
-// 	mlx_loop(mlx);
-// 	mlx_terminate(mlx);
-// 	return (EXIT_SUCCESS);
-// }	mlxx_t;
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <memory.h>
-#include <time.h>
-
-// void	draw_map(int **map, int size, int x, int y);
-// void put_rect(int size, int *x,int *y);
-static mlx_image_t	*g_img;
-float raduis = 1;
 
 void ft_update(void *param);
+
+void put_lines(t_player *player, int x, int y);
+
 int get_rgba(int r, int g, int b, int a)
 {
     return (r << 24 | g << 16 | b << 8 | a);
 }
 void map(float *x_r, float *y_r, int x, int y)
 {
-	*x_r = (float)x /(WIDTH/2) - 1.0;
-	*y_r = (float)y /(HEIGHT/2) - 1.0; 
+	*x_r = (float)x /(WIDTH/4) - 2.0;
+	*y_r = (float)y /(HEIGHT/4) - 2.0; 
 }
 
 void mlx_clear_image( mlx_image_t* image)
 {
 	int i, j;
 	i = 0;
-	while (i < image->width)
+	while (i < MWIDTH)
 	{
 		j = 0;
-		while (j < image->height)
+		while (j < MHEIGHT)
 		{
 			mlx_put_pixel(image, i, j,0);
 			j++;
@@ -144,42 +69,64 @@ int circel(float x_r, float y_r, float raduis, t_player *player)
 
 int is_awall(t_player *player, float x, float y)
 {
-	if (player->map[(int)x/32][(int)y/32] == '1')
+	if (x < 0 || y < 0 || x > MWIDTH || y > MHEIGHT)
+		return (1);
+	else if  (player->map[(int)x/player->size[0]][(int)y/player->size[1]] == '0')
+		return (0);
+	else if ( abs(player->x - player->minX) >=1 && abs(player->y - player->minY) >= 1 )
 		return (1);
 	else
-		return (0);
+		return (1);
 }
-
-void update(t_player *player){
-	float x , y;
-	player->rotation_angle += player->turn_direction * player->rot_speed;
-	x = player->x + player->walk_direction[0] * cos(player->rotation_angle) * player->move_speed;
-	y = player->y + player->walk_direction[0]  * sin(player->rotation_angle) * player->move_speed;
-	// if (player->walk_direction[1])
-	// {
-	// 	x = player->x + player->walk_direction[1] * cos(0.5 * M_PI -  player->rotation_angle) * player->move_speed;
-	// 	y = player->y + player->walk_direction[1] * sin(0.5 * M_PI -  player->rotation_angle) * player->move_speed;
-	// }
-	// if (!is_awall(player, x, y))
-	// {
-	// 	player->x = x;
-	// 	player->y = y;
-	// }
-}
-
-
 void put_line(t_player  *player)
 {
 	float maxX, maxY;
+	float step = 0.001;
 	int i = 0;
-	maxX = (float)player->x + cos(player->rotation_angle);
-	maxY = (float)player->y + sin(player->rotation_angle);
-	while (i < 30)
+	maxX = (float)player->x;
+	maxY = (float)player->y;
+	float alpha = player->rotation_angle + M_PI/6;
+	while ( alpha > player->rotation_angle )
 	{
-		mlx_put_pixel(g_img,maxX,maxY, get_rgba(255,0,0,255));
-		maxX += cos(player->rotation_angle);
-		maxY += sin(player->rotation_angle);
-		i++;
+		i = 0;
+		maxX = (float)player->x;
+		maxY = (float)player->y;
+		while (i < player->raduis)
+		{
+			maxX += cos(alpha);
+			maxY += sin(alpha);
+			if (is_awall(player, maxX, maxY))
+			{
+				if (alpha - step <= player->rotation_angle )
+				{
+					player->minX = maxX;
+					player->minY = maxY;
+				}
+				break;
+			}
+			mlx_put_pixel(player->image,maxX,maxY, get_rgba(255,0,0,255));
+			i++;
+		}
+		alpha-= step;
+	}
+	alpha = player->rotation_angle - M_PI/6;
+	while ( alpha < player->rotation_angle )
+	{
+		i = 0;
+		maxX = (float)player->x;
+		maxY = (float)player->y;
+		while (i < player->raduis)
+		{
+			maxX += cos(alpha);
+			maxY += sin(alpha);
+			if (is_awall(player, maxX, maxY))
+			{
+				break;
+			}
+			mlx_put_pixel(player->image,maxX,maxY, get_rgba(255,0,0,255));
+			i++;
+		}
+		alpha +=step;
 	}
 }
 
@@ -189,42 +136,42 @@ void put_player(t_player *player)
 	int x = 0;
 	int y ; 
 
-	while (x < g_img->width)
+	while (x < MWIDTH)
 	{
 		y = 0;
-		while ( y < g_img->height)
+		while ( y < MHEIGHT)
 		{
 			map(&x_r, &y_r, x, y);
 			if (circel(x_r, y_r, 0.02, player))
-				mlx_put_pixel(g_img, x, y, get_rgba(255,0,0,255));
+				mlx_put_pixel(player->image, x, y, get_rgba(200,150,0,255));
 			else
-				mlx_put_pixel(g_img, x, y, get_rgba(0,0,0,0));
+				mlx_put_pixel(player->image, x, y, get_rgba(0,0,0,0));
 			y++;
 		}
 		x++;
 	}
 	put_line(player);
 }
-void put_rect(int size, int x,int y, int color)
+void put_rect(t_player *player, int x,int y, int color)
 {
-	int col = x + size;
-	int line = y + size;
+	int col = x + player->size[0];
+	int line = y + player->size[1];
 	int tmp= y;
-	while (x < col && x < WIDTH)
+	while (x < col && x < MWIDTH)
 	{
 		y = tmp;
-		while (y < line && y < HEIGHT)
+		while (y < line && y < MHEIGHT)
 		{
 			if (color == 1)
-				mlx_put_pixel(g_img, x, y, get_rgba(0, 0, 0,255));
+				mlx_put_pixel(player->image, x, y, get_rgba(0, 0, 0,255));
 			else
-				mlx_put_pixel(g_img, x, y, get_rgba(155, 155, 255, 255));
+				mlx_put_pixel(player->image, x, y, get_rgba(0, 155, 0, 255));
 			y++;
 		}
 		x++;
 	}
 }
-void	draw_map(char **map, int size, int x, int y)
+void	draw_map(t_player * player, int x, int y)
 {
 	int i=0;
 	int j=0;
@@ -234,12 +181,12 @@ void	draw_map(char **map, int size, int x, int y)
 		j = 0;
 		while (j < 15)
 		{
-			if (map[i][j] == '1')
-				put_rect(size, x, y, 1);
+			if (player->map[i][j] == '1')
+				put_rect(player, x, y, 1);
 			j++;
-			y +=size;
+			y +=player->size[1];
 		}
-		x +=size;
+		x +=player->size[0];
 		i++;
 	}
 }
@@ -247,168 +194,122 @@ void	draw_map(char **map, int size, int x, int y)
 void my_keyhook(void* param)
 {
 	t_player *player;
-char *map[18] = {
-		"111111111111111",
-        "100000000000101",
-        "100001100000101",
-        "111100000010101",
-        "100000000010101",
-        "100000001111101",
-        "100000000000001",
-        "100000000000001",
-        "111111000111101",
-        "100000000000001",
-        "111111111111111"
-		};
 	int x =0, y=0;
 	int check = 0;
 	player = param;
 	if (mlx_is_key_down(player->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(player->mlx);
 	if (mlx_is_key_down(player->mlx, MLX_KEY_D))
-	{
 		player->walk_direction[1] = 1;
-		ft_update(param);
-		check = 1;
-	}
-	// if (keydata.key == MLX_KEY_D && keydata.action == MLX_RELEASE)
-	// 	player->walk_direction[1] = 0;
 	if (mlx_is_key_down(player->mlx, MLX_KEY_RIGHT))
 		player->turn_direction = 1;
-	// if (keydata.key == MLX_KEY_RIGHT && keydata.action == MLX_RELEASE)
-	// 	player->turn_direction = 0;
 	if (mlx_is_key_down(player->mlx, MLX_KEY_A))
-	{
 		player->walk_direction[1] = -1;
-		ft_update(param);
-		check = 1;
-	}
-	// if (keydata.key == MLX_KEY_A && keydata.action == MLX_RELEASE)
-	// 	player->walk_direction[1] = 0;
 	if (mlx_is_key_down(player->mlx, MLX_KEY_LEFT))
-	{
 		player->turn_direction = -1;
-		ft_update(param);
-		check = 1;
-	}
 	if (mlx_is_key_down(player->mlx, MLX_KEY_RIGHT))
-	{
 		player->turn_direction = 1;
-		ft_update(param);
-		check = 1;
-	}
-	// if (keydata.key == MLX_KEY_LEFT && keydata.action == MLX_RELEASE)
-	// 	player->turn_direction = 0;
 	if (mlx_is_key_down(player->mlx, MLX_KEY_S))
-	{
 		player->walk_direction[0] = -1;
-		ft_update(param);
-		check = 1;
-	}
-		if (mlx_is_key_down(player->mlx, MLX_KEY_W))
-	{
-		player->walk_direction[0] = 1;
-		ft_update(param);
-		check = 1;
-	}
-	// if (keydata.key == MLX_KEY_S && keydata.action == MLX_RELEASE)
-	// 	player->walk_direction[0] = 0
+	if (mlx_is_key_down(player->mlx, MLX_KEY_W))
+			player->walk_direction[0] = 1;
+	ft_update(param);
 }
-
-// void	hook(void* param)
-// {
-// 	t_player* player;
-// 		char *map[18] = {
-// 		"111111111111111",
-//         "100000000000101",
-//         "100001000000101",
-//         "111100000010101",
-//         "100000000010101",
-//         "100000001111101",
-//         "100000000000001",
-//         "100000000000001",
-//         "111111000111101",
-//         "100000000000001",
-//         "111111111111111"
-// 		};
-// 	int x =0, y=0;
-// 	player = param;
-// 	if (mlx_is_key_down(player->mlx, MLX_KEY_ESCAPE))
-// 		mlx_close_window(player->mlx);
-// 	if (mlx_is_key_down(player->mlx, MLX_KEY_P))
-// 		mlx_delete_image(player->mlx, g_img);
-// 	if (mlx_is_key_down(player->mlx, MLX_KEY_UP))
-// 		player->walk_direction = 1;
-// 	if (mlx_is_key_down(player->mlx, MLX_KEY_RIGHT))
-// 		player->turn_direction = 1;
-// 	if (mlx_is_key_down(player->mlx, MLX_KEY_DOWN))
-// 		player->walk_direction = -1;
-// 	if (mlx_is_key_down(player->mlx, MLX_KEY_LEFT))
-// 		player->turn_direction = -1;
-// 	update(player);
-// 	draw_map(map, 32, x, y);
-// 	put_player(player);
-// }
-
 void ft_update(void *param)
 {
-		char *map[18] = {
-		"111111111111111",
-        "100000000000101",
-        "100001100000101",
-        "111100000010101",
-        "100000000010101",
-        "100000001111101",
-        "100000000000001",
-        "100000000000001",
-        "111111000111101",
-        "100000000000001",
-        "111111111111111"
-		};
 	t_player *player = param;
 	float x , y;
+	int i = 0;
+	int j = 0;
 	player->rotation_angle += player->turn_direction * player->rot_speed;
 	x = player->x + player->walk_direction[0] * cos(player->rotation_angle) * player->move_speed;
 	y = player->y + player->walk_direction[0]  * sin(player->rotation_angle) * player->move_speed;
-	// if (player->walk_direction[1])
-	// {
-	// 	x = player->x + player->walk_direction[1] * cos(0.5 * M_PI -  player->rotation_angle) * player->move_speed;
-	// 	y = player->y + player->walk_direction[1] * sin(0.5 * M_PI -  player->rotation_angle) * player->move_speed;
-	// }
-	if (!is_awall(player, x, y))
+	x += player->walk_direction[1] * sin(player->rotation_angle)*player->move_speed;
+	y +=  (-1) * player->walk_direction[1] * cos(player->rotation_angle)*player->move_speed;
+	if (!is_awall(player,x,y))
 	{
-		player->x = x;
-		player->y = y;
+		player->x = round(x);
+		player->y = round(y);
 	}
-	mlx_clear_image(g_img);
+	mlx_clear_image(player->image);
 	put_player(player);
-	draw_map(map, 32, 0, 0);
+	draw_map(player, 0, 0);
+	put_lines(player, 0, 0);
+	printf(" x    %d  y   %d \n", player->minX, player->minY);
+	printf(" px    %d  py   %d \n", player->x, player->y);
+	while (i < 4)
+	{
+		j = 0;
+		while (j < 4)
+		{
+			mlx_put_pixel(player->image, player->minX + i, player->minY + j, get_rgba(255,255,255,255));
+			j++;
+		}
+		i++;
+	}
 	player->turn_direction = 0;
 	player->walk_direction[0] = 0;
 	player->walk_direction[1] = 0;
 }
+
+void put_lines(t_player *player, int x, int y)
+{
+	while (x < 11 * player->size[0] )
+	{
+		if (x%player->size[0] == 0)
+		{
+			y = 0;
+			while (y < 15 * player->size[0])
+			{
+				mlx_put_pixel(player->image, x, y, get_rgba(155, 155, 155,255));
+				y++;
+			}
+		}
+		x++;
+	}
+	y = 0;
+		while (y < 15 * player->size[0] )
+	{
+		if (y%player->size[0] == 0)
+		{
+			x = 0;
+			while (x < 11 * player->size[0])
+			{
+				mlx_put_pixel(player->image, x, y, get_rgba(155, 155, 155,255));
+				x++;
+			}
+		}
+		y++;
+	} 
+}
 int32_t	main(void)
 {
 	t_player player;
+	int	size[2];
 
-	player.x = (11 * 32) /2;
-	player.y = (15 * 32) /2;
+	int x=0, y = 0;
+	size[0] = 24;
+	size[1] = 24;
+	player.x = (11 * size[0]) /2;
+	player.y = (15 * size[1]) /2;
 	player.turn_direction = 0;
 	player.walk_direction[0] = 0;
 	player.walk_direction[1] = 0;
 	player.rotation_angle = M_PI / 2;
-	player.move_speed = 5.0;
-	player.rot_speed = 5 * M_PI / 180;
-
-	int x=0, y = 0;
-		char *map[18] = {
+	player.move_speed = 2.0;
+	player.rot_speed = 2 * M_PI / 180;
+	player.size = size;
+	player.minX = 0;
+	player.minY = 0;
+	player.raduis = MHEIGHT; //potentially player.raduis= rayCast_raduis();
+	char *map[18] = {
 		"111111111111111",
         "100000000000101",
         "100001100000101",
         "111100000010101",
-        "100000000010101",
-        "100000001111101",
-        "100000000000001",
+        "100001000010101",
+        "100010101111101",
+        "100000100000001",
         "100000000000001",
         "111111000111101",
         "100000000000001",
@@ -418,10 +319,10 @@ int32_t	main(void)
 	player.mlx = mlx_init(WIDTH, HEIGHT, "MLX42", false);
 	if (!player.mlx)
 		exit(EXIT_FAILURE);
-	g_img = mlx_new_image(player.mlx, WIDTH, HEIGHT);
-	mlx_image_to_window(player.mlx, g_img, 0, 0);
+	player.image = mlx_new_image(player.mlx, WIDTH, HEIGHT);
+	mlx_image_to_window(player.mlx, player.image, 0, 0);
 	put_player(&player);
-	draw_map(map, 32, 0, 0);
+	draw_map(&player, 0, 0);
 	// mlx_key_hook(player.mlx, &my_keyhook, &player);
 	mlx_loop_hook(player.mlx, my_keyhook, &player);
 	//mlx_loop_hook(player.mlx, ft_update, &player);
